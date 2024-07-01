fea8aff91edff089218246bbfd9535d9f6dffa51
jae woo Jung <awfjol2008@gmail.com>
2024-07-01 21:35:45 +0900

    chore: 스프링 프로젝트 생성

---
+++ Inside-Out/src/main/java/com/goorm/insideout/InsideOutApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.insideout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsideOutApplication.class, args);
+	}
+
+}
