59a17d7d96d63afe87b276634f1d119be7834068
jae woo Jung <awfjol2008@gmail.com>
2024-07-11 18:27:58 +0900

    feat: 기존 API 응답 모델에 에러 관련 필드 추가

--- Inside-Out/src/main/java/com/goorm/insideout/global/response/ApiResponse.java
+++ Inside-Out/src/main/java/com/goorm/insideout/global/response/ApiResponse.java
@@ -3,29 +3,36 @@ package com.goorm.insideout.global.response;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.goorm.insideout.global.exception.ErrorCode;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 public class ApiResponse<T> {
+	private ErrorCode errorCode;
+
 	@JsonInclude(JsonInclude.Include.NON_EMPTY)
 	private Metadata metadata; // 주로 결과의 개수를 담는다.
 
 	@JsonInclude(JsonInclude.Include.NON_EMPTY)
 	private List<T> results; // 여러 개의 결과를 반환할 때 사용한다.
 
-	@JsonInclude(JsonInclude.Include.NON_EMPTY)
-	private T data; // 단일 데이터 객체나 기타 응답 데이터를 담는 필드이다.
-
 	public ApiResponse(List<T> results) {
+		this.errorCode = ErrorCode.REQUEST_OK;
 		this.metadata = new Metadata(results.size());
 		this.results = results;
 	}
 
 	public ApiResponse(T data) {
+		this.errorCode = ErrorCode.REQUEST_OK;
 		this.metadata = new Metadata(1);
 		this.results = List.of(data);
+	}
+
+	public ApiResponse(ErrorCode errorCode) {
+		this.errorCode = errorCode;
+
 	}
 
 	@Getter
