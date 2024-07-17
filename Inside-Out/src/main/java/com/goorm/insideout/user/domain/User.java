53defe82188eabb029af206b53edd2c521a28e6c
SeongJunChun <146021359+SeongJunChun@users.noreply.github.com>
2024-07-17 19:08:10 +0900

    feat: 로그인 구현 완료 로그인시 사용자 정보 인증후 토큰을 발급하는 로직과 토큰으로 사용자 정보를 확인해 인가 정보에 등록하는 로직을 작성했습니다

--- Inside-Out/src/main/java/com/goorm/insideout/user/domain/User.java
+++ Inside-Out/src/main/java/com/goorm/insideout/user/domain/User.java
@@ -6,10 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Entity
 @Setter
@@ -29,4 +33,5 @@ public class User {
 
 	@Column(nullable = false)
 	private String name;
+
 }
