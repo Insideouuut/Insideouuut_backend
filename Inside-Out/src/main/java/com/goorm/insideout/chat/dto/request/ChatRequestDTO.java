e0071d80c450012241f727b7dc3fa842111f37d3
jeongdong99 <sjd0219@gmail.com>
2024-07-19 07:15:31 +0900

    feat: chat 도메인과 리포지토리 구현

---
+++ Inside-Out/src/main/java/com/goorm/insideout/chat/dto/request/ChatRequestDTO.java
@@ -0,0 +1,13 @@
+package com.goorm.insideout.chat.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatRequestDTO {
+	private Long userId;
+	private String content;
+
+}
+
---
+++ Inside-Out/src/main/java/com/goorm/insideout/chat/dto/response/ChatResponseDTO.java
@@ -0,0 +1,4 @@
+package com.goorm.insideout.chat.dto.response;
+
+public class ChatResponseDTO {
+}
