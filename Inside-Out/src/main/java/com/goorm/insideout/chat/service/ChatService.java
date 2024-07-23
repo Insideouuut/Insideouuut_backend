9f289d70c10a2962dea3125be4ef865ef776a07d
jeongdong99 <sjd0219@gmail.com>
2024-07-23 14:57:44 +0900

    refactor: JWT 적용과 Transactional

--- Inside-Out/src/main/java/com/goorm/insideout/chat/service/ChatService.java
+++ Inside-Out/src/main/java/com/goorm/insideout/chat/service/ChatService.java
@@ -1,14 +1,18 @@
 package com.goorm.insideout.chat.service;
 
+import java.security.Principal;
 import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.goorm.insideout.chat.domain.Chat;
 import com.goorm.insideout.chat.dto.request.ChatRequestDTO;
 import com.goorm.insideout.chat.repository.ChatRepository;
 import com.goorm.insideout.chatroom.domain.ChatRoom;
 import com.goorm.insideout.chatroom.repository.ChatRoomRepository;
+import com.goorm.insideout.global.exception.ErrorCode;
+import com.goorm.insideout.global.exception.ModongException;
 import com.goorm.insideout.user.domain.User;
 import com.goorm.insideout.user.repository.UserRepository;
 
@@ -21,26 +25,38 @@ public class ChatService {
 	private final ChatRoomRepository chatRoomRepository;
 	private final ChatRepository chatRepository;
 	private final UserRepository userRepository;
-
-	public void createChat(Long roomId, ChatRequestDTO message) {
+	@Transactional
+	public Chat createChat(Long roomId, ChatRequestDTO message, Principal principal) {
 
 		// 채팅을 작성한 작성자 찾기
-		User sender = userRepository.findById(message.getUserId())
-			.orElseThrow(() -> new RuntimeException("User not found"));
+		User sender = getSender(principal.getName());
 
 		// 채팅방 찾기
-		ChatRoom chatRoom = chatRoomRepository.findById(roomId)
-			.orElseThrow(() -> new RuntimeException("Chat room not found"));
+		ChatRoom chatRoom = getChatRoom(roomId);
 
 		// 채팅 엔티티 생성
-		LocalDateTime now = LocalDateTime.now();
+
 		Chat chat = Chat.builder()
 			.content(message.getContent())
 			.chatRoom(chatRoom)
 			.user(sender)
-			.sendTime(now)
+			.sendTime(LocalDateTime.now())
 			.build();
-		chatRepository.save(chat);
+		return chatRepository.save(chat);
 
 	}
+
+	private User getSender(String email) {
+		//해당 이메일로 가입한 유저가 존재하는지 확인
+		return userRepository.findByEmail(email)
+			.orElseThrow(() -> ModongException.from(ErrorCode.USER_NOT_FOUND));
+	}
+
+	private ChatRoom getChatRoom(Long roomId) {
+		//해당 PK에 맞는 채팅방 검색
+		return chatRoomRepository.findById(roomId)
+			.orElseThrow(() -> ModongException.from(ErrorCode.CHATROOM_NOT_FOUND));
+
+	}
+
 }
