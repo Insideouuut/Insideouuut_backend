4028166848abcf2e743c363225a91abc7360b444
jae woo Jung <awfjol2008@gmail.com>
2024-07-23 17:31:17 +0900

    feat: 모임 엔티티 조회수 및 좋아요 관련 필드 추가

--- Inside-Out/src/main/java/com/goorm/insideout/meeting/domain/Meeting.java
+++ Inside-Out/src/main/java/com/goorm/insideout/meeting/domain/Meeting.java
@@ -5,16 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.goorm.insideout.image.domain.Image;
+import com.goorm.insideout.like.domain.MeetingLike;
+import com.goorm.insideout.user.domain.User;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 
 @Entity
@@ -33,6 +37,9 @@ public class Meeting {
 
 	@Column(name = "description")
 	private String description;
+
+	@Column(name = "view", columnDefinition = "integer default 0", nullable = false)
+	private int view;
 
 	@Column(name = "rule")
 	private String rule;
@@ -60,7 +67,7 @@ public class Meeting {
 	private int maleRatio;
 
 	@Column(name = "femail_ratio")
-	private long femaleRatio;
+	private int femaleRatio;
 
 	@Column(name = "has_membership_fee")
 	private boolean hasMembershipFee;
@@ -79,4 +86,11 @@ public class Meeting {
 
 	@OneToMany(mappedBy = "meeting", cascade = CascadeType.ALL)
 	private List<Image> images = new ArrayList<>();
+
+	@OneToMany(mappedBy = "meeting", cascade = CascadeType.ALL)
+	private List<MeetingLike> likes = new ArrayList<>();
+
+	// User 엔티티를 구현하지 않았으므로 임시 주석 처리
+	// @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	// private User author;
 }
