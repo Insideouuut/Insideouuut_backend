5ba1974e7da9c1870167866bcaafad0e4ffceeb2
jae woo Jung <awfjol2008@gmail.com>
2024-07-23 17:24:13 +0900

    feat: QueryDslConfig 구현
    QueryDsl 사용에 필요한 Config 구현

---
+++ Inside-Out/src/main/java/com/goorm/insideout/global/config/QueryDslConfig.java
@@ -0,0 +1,20 @@
+package com.goorm.insideout.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Configuration
+public class QueryDslConfig {
+	@PersistenceContext
+	private EntityManager em;
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(em);
+	}
+}
