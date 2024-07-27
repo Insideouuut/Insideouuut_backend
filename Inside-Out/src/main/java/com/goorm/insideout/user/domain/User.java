b148429520a03a7c83229649bb8945167b0b5d9f
jae woo Jung <awfjol2008@gmail.com>
2024-07-27 20:30:39 +0900

    feat: 사용자 엔티티에 모임 목록 관련 필드 추가

--- Inside-Out/src/main/java/com/goorm/insideout/user/domain/User.java
+++ Inside-Out/src/main/java/com/goorm/insideout/user/domain/User.java
@@ -1,10 +1,17 @@
 package com.goorm.insideout.user.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.goorm.insideout.meeting.domain.Meeting;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,5 +40,8 @@ public class User {
 
 	@Column(nullable = false)
 	private String name;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Meeting> meetings = new ArrayList<>();
 
 }
