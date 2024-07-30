505caf9cb15919289ffb6ca9657c315edb54a79a
jeongdong99 <sjd0219@gmail.com>
2024-07-30 22:28:52 +0900

    refactor: 리펙토링과 조회 기능 추가

--- Inside-Out/src/main/java/com/goorm/insideout/chat/service/ChatService.java
+++ Inside-Out/src/main/java/com/goorm/insideout/chat/service/ChatService.java
@@ -1,13 +1,15 @@
 package com.goorm.insideout.chat.service;
 
-import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.goorm.insideout.chat.domain.Chat;
 import com.goorm.insideout.chat.dto.request.ChatRequestDTO;
+import com.goorm.insideout.chat.dto.response.ChatResponseDTO;
 import com.goorm.insideout.chat.repository.ChatRepository;
 import com.goorm.insideout.chatroom.domain.ChatRoom;
 import com.goorm.insideout.chatroom.repository.ChatRoomRepository;
@@ -15,6 +17,7 @@ import com.goorm.insideout.global.exception.ErrorCode;
 import com.goorm.insideout.global.exception.ModongException;
 import com.goorm.insideout.user.domain.User;
 import com.goorm.insideout.user.repository.UserRepository;
+import com.goorm.insideout.userchatroom.repository.UserChatRoomRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,41 +25,63 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChatService {
 
-	private final ChatRoomRepository chatRoomRepository;
 	private final ChatRepository chatRepository;
+	private final ChatRoomRepository chatRoomRepository;
+	private final UserChatRoomRepository userChatRoomRepository;
 	private final UserRepository userRepository;
+
+	// 채팅 생성
 	@Transactional
-	public Chat createChat(Long roomId, ChatRequestDTO message, Principal principal) {
-
-		// 채팅을 작성한 작성자 찾기
-		User sender = getSender(principal.getName());
-
-		// 채팅방 찾기
+	public Chat createChat(Long roomId, ChatRequestDTO message, String email) {
 		ChatRoom chatRoom = getChatRoom(roomId);
 
-		// 채팅 엔티티 생성
+		User user = userRepository.findByEmail(email)
+			.orElseThrow(() -> ModongException.from(ErrorCode.CHATROOM_NOT_FOUND));
 
 		Chat chat = Chat.builder()
 			.content(message.getContent())
 			.chatRoom(chatRoom)
-			.user(sender)
+			.user(user)
 			.sendTime(LocalDateTime.now())
 			.build();
+
+		updateChatRoomLastMessage(chatRoom, message.getContent());
 		return chatRepository.save(chat);
-
 	}
 
-	private User getSender(String email) {
-		//해당 이메일로 가입한 유저가 존재하는지 확인
-		return userRepository.findByEmail(email)
-			.orElseThrow(() -> ModongException.from(ErrorCode.USER_NOT_FOUND));
+	// 읽지 않은 메시지 조회
+	@Transactional(readOnly = true)
+	public List<ChatResponseDTO> getUnreadMessages(Long userId, Long chatRoomId) {
+		LocalDateTime lastVisitedTime = userChatRoomRepository.findConfigTime(userId, chatRoomId);
+		List<Chat> chats = (lastVisitedTime == null) ?
+			chatRepository.findAllByChatRoomId(chatRoomId) :
+			chatRepository.findAllUnreadMessages(chatRoomId, lastVisitedTime);
+
+		return chats.stream()
+			.map(this::convertToChatResponseDTO)
+			.collect(Collectors.toList());
 	}
 
+	// 채팅방 가져오기
 	private ChatRoom getChatRoom(Long roomId) {
-		//해당 PK에 맞는 채팅방 검색
 		return chatRoomRepository.findById(roomId)
 			.orElseThrow(() -> ModongException.from(ErrorCode.CHATROOM_NOT_FOUND));
+	}
 
+	// 채팅방 마지막 메시지 업데이트
+	private void updateChatRoomLastMessage(ChatRoom chatRoom, String messageContent) {
+		chatRoom.setLastMessageContent(messageContent);
+		chatRoom.setLastMessageTime(LocalDateTime.now());
+		chatRoomRepository.save(chatRoom);
+	}
+
+	// Chat 엔티티를 ChatResponseDTO로 변환
+	private ChatResponseDTO convertToChatResponseDTO(Chat chat) {
+		return ChatResponseDTO.builder()
+			.content(chat.getContent())
+			.sendTime(chat.getSendTime())
+			.sender(chat.getUser().getName())
+			.build();
 	}
 
 }
