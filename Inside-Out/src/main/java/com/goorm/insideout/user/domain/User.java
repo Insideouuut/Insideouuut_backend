f3276f9749b19db5892648d6dae10208bd60da99
SeongJunChun <146021359+SeongJunChun@users.noreply.github.com>
2024-07-31 01:19:14 +0900

    feat: 사용자 엔티티에 기타 정보 추가

--- Inside-Out/src/main/java/com/goorm/insideout/user/domain/User.java
+++ Inside-Out/src/main/java/com/goorm/insideout/user/domain/User.java
@@ -1,10 +1,21 @@
 package com.goorm.insideout.user.domain;
 
+import java.time.LocalDate;
+import java.util.Set;
+
+import com.goorm.insideout.meeting.domain.Category;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,4 +44,20 @@ public class User {
 	@Column(nullable = false)
 	private String name;
 
+	private String profileImage;
+
+	private String nickname;
+
+	private String location;
+
+	private LocalDate birthDate;
+
+	private String phoneNumber;
+
+	@ElementCollection(targetClass = Category.class, fetch = FetchType.LAZY)
+	@Enumerated(EnumType.STRING)
+	private Set<Category> interests;
+
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
 }
