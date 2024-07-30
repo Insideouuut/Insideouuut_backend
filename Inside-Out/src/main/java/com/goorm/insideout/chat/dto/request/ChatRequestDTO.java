cd1c1b37d954763c9fedac31d925e05ac2e47825
jeongdong99 <sjd0219@gmail.com>
2024-07-30 22:25:24 +0900

    feat: chatroom 생성에 따른 채팅방의 chats 로그 가져오는 기능 구현

--- Inside-Out/src/main/java/com/goorm/insideout/chat/dto/request/ChatRequestDTO.java
+++ Inside-Out/src/main/java/com/goorm/insideout/chat/dto/request/ChatRequestDTO.java
@@ -1,10 +1,12 @@
 package com.goorm.insideout.chat.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ChatRequestDTO {
 	private String content;
 }
--- Inside-Out/src/main/java/com/goorm/insideout/chat/repository/ChatRepository.java
+++ Inside-Out/src/main/java/com/goorm/insideout/chat/repository/ChatRepository.java
@@ -1,9 +1,25 @@
 package com.goorm.insideout.chat.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.goorm.insideout.chat.domain.Chat;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+	// 채팅방의 전체 메시지 개수를 세는 메서드
+	long countByChatRoomId(Long chatRoomId);
 
+	// 특정 시간 이후의 메시지 개수를 세는 메서드
+	long countByChatRoomIdAndSendTimeAfter(Long chatRoomId, LocalDateTime lastVisitedTime);
+
+	List<Chat> findAllByChatRoomId(Long chatRoomId);
+
+	// 특정 시간 이후의 읽지 않은 메시지 목록을 조회하는 메서드
+	@Query("SELECT c FROM Chat c WHERE c.chatRoom.id = :chatRoomId AND c.sendTime > :lastVisitedTime")
+	List<Chat> findAllUnreadMessages(@Param("chatRoomId") Long chatRoomId, @Param("lastVisitedTime") LocalDateTime lastVisitedTime);
 }
+
