ed537494eae1808c732e5d75cd20a5a914d08f6b
SeongJunChun <146021359+SeongJunChun@users.noreply.github.com>
2024-07-31 01:20:59 +0900

    feat: 회원 가입 로직 수정및 이메일,닉네임 중복 확인 메서드 생성

--- Inside-Out/src/main/java/com/goorm/insideout/user/service/UserJoinService.java
+++ Inside-Out/src/main/java/com/goorm/insideout/user/service/UserJoinService.java
@@ -1,16 +1,23 @@
 package com.goorm.insideout.user.service;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
 
 import com.goorm.insideout.global.exception.ErrorCode;
 import com.goorm.insideout.global.exception.ModongException;
+import com.goorm.insideout.meeting.domain.Category;
+import com.goorm.insideout.user.domain.Gender;
 import com.goorm.insideout.user.domain.User;
+import com.goorm.insideout.user.dto.request.CheckEmailRequest;
+import com.goorm.insideout.user.dto.request.CheckNicknameRequest;
 import com.goorm.insideout.user.dto.request.UserJoinRequestDTO;
 import com.goorm.insideout.user.repository.UserRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -19,21 +26,38 @@ public class UserJoinService {
 	private final UserRepository userRepository;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+	@Transactional
 	public void join(UserJoinRequestDTO userJoinRequest) {
-
-		validateExistUser(userJoinRequest);
+		List<String> categoryDTO = userJoinRequest.getCategory();
+		Set<Category> categorySet = new HashSet<>();
+		for (String category : categoryDTO) {
+			categorySet.add(Category.valueOf(category));
+		}
+		validateExistEmail(userJoinRequest.getEmail());
+		validateExistNickname(userJoinRequest.getName());
 		User joinUser = User.builder()
 			.email(userJoinRequest.getEmail())
 			.password(bCryptPasswordEncoder.encode(userJoinRequest.getPassword()))
 			.name(userJoinRequest.getName())
+			.birthDate(userJoinRequest.getBirthDate())
+			.phoneNumber(userJoinRequest.getPhoneNumber())
+			.interests(categorySet)
+			.gender(Gender.valueOf(userJoinRequest.getGender()))
+			.location(userJoinRequest.getLocation())
+			.nickname(userJoinRequest.getNickName())
 			.build();
 		userRepository.save(joinUser);
 	}
 
-	private void validateExistUser(UserJoinRequestDTO userJoinRequest) {
-		String email = userJoinRequest.getEmail();
+	public void validateExistEmail(String email) {
 		if (userRepository.existsByEmail(email)) {
 			throw ModongException.from(ErrorCode.DUPLICATE_USER_EMAIL);
+		}
+	}
+
+	public void validateExistNickname(String nickname) {
+		if (userRepository.existsByEmail(nickname)) {
+			throw ModongException.from(ErrorCode.DUPLICATE_USER_NICKNAME);
 		}
 	}
 }
