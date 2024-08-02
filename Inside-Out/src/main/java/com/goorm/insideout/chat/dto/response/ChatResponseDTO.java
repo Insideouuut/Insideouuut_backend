f3cc3200f32994377e303ce4b0925f2e90d61aad
jeongdong99 <sjd0219@gmail.com>
2024-08-02 14:49:14 +0900

    fix : @EqualsAndHashCode 추가

--- Inside-Out/src/main/java/com/goorm/insideout/chat/dto/response/ChatResponseDTO.java
+++ Inside-Out/src/main/java/com/goorm/insideout/chat/dto/response/ChatResponseDTO.java
@@ -11,26 +11,10 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@EqualsAndHashCode
 public class ChatResponseDTO {
 	private Long id;
 	private String content;
 	private LocalDateTime sendTime;
 	private String sender;
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		ChatResponseDTO that = (ChatResponseDTO)o;
-		return Objects.equals(content, that.content) &&
-			Objects.equals(sendTime, that.sendTime) &&
-			Objects.equals(sender, that.sender);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(content, sendTime, sender);
-	}
 }
