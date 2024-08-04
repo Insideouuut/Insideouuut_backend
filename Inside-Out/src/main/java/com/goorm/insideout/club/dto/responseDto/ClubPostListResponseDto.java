e4573c1cd53291b38b8cd4e7e61eec67c947651c
sunnybinny <yusun4756@gmail.com>
2024-08-04 16:35:17 +0900

    feat: 게시판 관련 dto 추가

--- Inside-Out/src/main/java/com/goorm/insideout/club/dto/responseDto/ClubPostListResponseDto.java
+++ Inside-Out/src/main/java/com/goorm/insideout/club/dto/responseDto/ClubPostListResponseDto.java
@@ -1,4 +1,19 @@
 package com.goorm.insideout.club.dto.responseDto;
 
+import com.goorm.insideout.club.entity.ClubPost;
+
 public class ClubPostListResponseDto {
+	private String postTitle;
+	private String writer;
+	private String content;
+
+	public static ClubPostListResponseDto of(ClubPost clubPost){
+		return new ClubPostListResponseDto(clubPost);
+	}
+
+	public ClubPostListResponseDto(ClubPost clubPost) {
+		this.postTitle = clubPost.getPostTitle();
+		this.writer = clubPost.getWriter();
+		this.content = clubPost.getPostContent();
+	}
 }
--- Inside-Out/src/main/java/com/goorm/insideout/club/dto/responseDto/ClubPostResponseDto.java
+++ Inside-Out/src/main/java/com/goorm/insideout/club/dto/responseDto/ClubPostResponseDto.java
@@ -1,4 +1,40 @@
 package com.goorm.insideout.club.dto.responseDto;
 
+import java.time.LocalDateTime;
+
+import com.goorm.insideout.club.entity.Club;
+import com.goorm.insideout.club.entity.ClubPost;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class ClubPostResponseDto {
+	private Long Id;
+
+	private Long clubId;
+
+	private String postTitle;
+
+	private String writer;
+
+	private LocalDateTime createTime;
+	private String postContent;
+
+
+	public static ClubPostResponseDto of(ClubPost clubPost){
+		ClubPostResponseDto res = new ClubPostResponseDto();
+
+		res.setId(clubPost.getId());
+		res.setClubId(clubPost.getClubId());
+		res.setPostTitle(clubPost.getPostTitle());
+		res.setWriter(clubPost.getWriter());
+		res.setCreateTime(clubPost.getCreateTime());
+		res.setPostContent(clubPost.getPostContent());
+
+		return res;
+	}
 }
