3a153780890c176994baccace4148910faa55069
jae woo Jung <awfjol2008@gmail.com>
2024-08-05 16:55:54 +0900

    feat: Swagger UI가 HTTPS 요청을 받을 수 있도록 설정 추가

--- Inside-Out/src/main/java/com/goorm/insideout/global/config/SwaggerConfig.java
+++ Inside-Out/src/main/java/com/goorm/insideout/global/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.goorm.insideout.global.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,16 +10,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 	@Bean
 	public OpenAPI api() {
+		Server server = new Server();
+		server.setUrl("https://modong-backend.site"); // https://에 접근 가능하게 설정
+
 		return new OpenAPI()
 			// 보안 관련
 			.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
 			.components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
-			.info(apiInfo());
+			.info(apiInfo())
+			.servers(List.of(server));
 	}
 
 	private Info apiInfo() {
