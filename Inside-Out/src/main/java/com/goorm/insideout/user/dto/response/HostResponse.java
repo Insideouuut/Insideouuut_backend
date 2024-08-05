191317af512284a5b414163f798d3b977ba17bfa
jeongdong99 <sjd0219@gmail.com>
2024-08-05 15:06:38 +0900

    test : 테스트 코드 수정

--- Inside-Out/src/main/java/com/goorm/insideout/user/dto/response/HostResponse.java
+++ Inside-Out/src/main/java/com/goorm/insideout/user/dto/response/HostResponse.java
@@ -2,9 +2,11 @@ package com.goorm.insideout.user.dto.response;
 
 import com.goorm.insideout.user.domain.User;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 public class HostResponse {
 	private Long id;
 	private String nickname;
