a6f43c6a63c163db19b51227167816f42e864f74
SeongJunChun <146021359+SeongJunChun@users.noreply.github.com>
2024-08-07 16:32:54 +0900

    feat: 모임 정보를 담을 dto 생성

---
+++ Inside-Out/src/main/java/com/goorm/insideout/user/dto/response/ProfileMeetingResponse.java
@@ -0,0 +1,20 @@
+package com.goorm.insideout.user.dto.response;
+
+import com.goorm.insideout.meeting.dto.response.MeetingResponse;
+
+import lombok.Getter;
+
+@Getter
+public class ProfileMeetingResponse {
+	private final String category;
+	private final String meetingName;
+	private final String meetingImage;
+	private final String meetingHostNickName;
+	public ProfileMeetingResponse(MeetingResponse meetingResponse){
+		this.category="MEETING";
+		this.meetingName=meetingResponse.getTitle();
+		this.meetingImage=meetingResponse.getImages().get(0).getUrl();
+		this.meetingHostNickName=meetingResponse.getHost().getNickname();
+	}
+
+}
