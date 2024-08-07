c972abd96b6048f76fc6423cca1b6c32cfb8e38f
jae woo Jung <awfjol2008@gmail.com>
2024-08-07 12:03:51 +0900

    feat: 동아리 이미지 변환 로직 추가

--- Inside-Out/src/main/java/com/goorm/insideout/image/dto/StoreImageDto.java
+++ Inside-Out/src/main/java/com/goorm/insideout/image/dto/StoreImageDto.java
@@ -1,5 +1,7 @@
 package com.goorm.insideout.image.dto;
 
+import com.goorm.insideout.club.entity.Club;
+import com.goorm.insideout.image.domain.ClubImage;
 import com.goorm.insideout.image.domain.MeetingImage;
 import com.goorm.insideout.image.domain.ProfileImage;
 import com.goorm.insideout.meeting.domain.Meeting;
@@ -9,6 +11,10 @@ public record StoreImageDto(String uploadName, String storeName) {
 
 	public ProfileImage toProfileImageEntity(User user, String url) {
 		return ProfileImage.createProfileImage(uploadName, storeName, url, user);
+	}
+
+	public ClubImage toClubImageEntity(Club club, String url) {
+		return ClubImage.createClubImage(uploadName, storeName, url, club);
 	}
 
 	public MeetingImage toMeetingImageEntity(Meeting meeting, String url) {
