8b438b8c6968c345656c78c5bf6616ce9bd4a317
jae woo Jung <awfjol2008@gmail.com>
2024-08-09 15:54:34 +0900

    feat: 동아리 검색 API 및 통합 검색 API 구현

--- Inside-Out/src/main/java/com/goorm/insideout/search/controller/SearchController.java
+++ Inside-Out/src/main/java/com/goorm/insideout/search/controller/SearchController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.goorm.insideout.club.dto.responseDto.ClubBoardResponseDto;
+import com.goorm.insideout.club.service.ClubService;
 import com.goorm.insideout.global.response.ApiResponse;
 import com.goorm.insideout.meeting.dto.request.SearchRequest;
 import com.goorm.insideout.meeting.dto.response.MeetingResponse;
 import com.goorm.insideout.meeting.service.MeetingService;
+import com.goorm.insideout.search.dto.response.UnifiedSearchResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,11 +22,12 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api")
 @Tag(name = "SearchController", description = "검색 관련 API")
 public class SearchController {
+	private final ClubService clubService;
 	private final MeetingService meetingService;
 
 	@GetMapping("/search/club")
 	@Operation(summary = "동아리 검색 API", description = "동아리를 검색하는 API 입니다.")
-	public ApiResponse<MeetingResponse> findClubs(
+	public ApiResponse<ClubBoardResponseDto> findClubs(
 		@RequestParam("query") String query,
 		@RequestParam("category") String category,
 		@RequestParam("sort") String sortType
@@ -31,10 +35,10 @@ public class SearchController {
 		SearchRequest condition = new SearchRequest(query, category, sortType);
 
 		if (query.equals("all") && category.equals("all")) {
-			return new ApiResponse<>(meetingService.findBySortType(condition));
+			return new ApiResponse<>(clubService.findBySortType(condition));
 		}
 
-		return new ApiResponse<>(meetingService.findByConditionAndSortType(condition));
+		return new ApiResponse<>(clubService.findByConditionAndSortType(condition));
 	}
 
 	@GetMapping("/search/meeting")
@@ -55,17 +59,28 @@ public class SearchController {
 
 	@GetMapping("/search/all")
 	@Operation(summary = "동아리 및 모임 검색 API", description = "동아리와 모임을 한꺼번에 검색하는 API 입니다.")
-	public ApiResponse<MeetingResponse> findAll(
+	public ApiResponse<UnifiedSearchResponse> findAll(
 		@RequestParam("query") String query,
 		@RequestParam("category") String category,
 		@RequestParam("sort") String sortType
 	) {
 		SearchRequest condition = new SearchRequest(query, category, sortType);
+		UnifiedSearchResponse unifiedSearchResponse;
 
 		if (query.equals("all") && category.equals("all")) {
-			return new ApiResponse<>(meetingService.findBySortType(condition));
+			unifiedSearchResponse = UnifiedSearchResponse.of(
+				clubService.findBySortType(condition),
+				meetingService.findBySortType(condition)
+			);
+
+			return new ApiResponse<>(unifiedSearchResponse);
 		}
 
-		return new ApiResponse<>(meetingService.findByConditionAndSortType(condition));
+		unifiedSearchResponse = UnifiedSearchResponse.of(
+			clubService.findByConditionAndSortType(condition),
+			meetingService.findByConditionAndSortType(condition)
+		);
+
+		return new ApiResponse<>(unifiedSearchResponse);
 	}
 }
