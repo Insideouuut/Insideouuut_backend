9fbb15033148ca513362e99b302f4882d941279c
jae woo Jung <awfjol2008@gmail.com>
2024-08-09 20:27:09 +0900

    feat: 동아리 게시글 API에 이미지 처리 기능 추가

--- Inside-Out/src/main/java/com/goorm/insideout/club/controller/ClubPostController.java
+++ Inside-Out/src/main/java/com/goorm/insideout/club/controller/ClubPostController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.goorm.insideout.auth.dto.CustomUserDetails;
 import com.goorm.insideout.chatroom.domain.ChatRoom;
@@ -31,6 +33,7 @@ import com.goorm.insideout.club.service.ClubPostService;
 import com.goorm.insideout.club.service.ClubUserService;
 import com.goorm.insideout.global.exception.ErrorCode;
 import com.goorm.insideout.global.response.ApiResponse;
+import com.goorm.insideout.image.service.ImageService;
 import com.goorm.insideout.user.domain.User;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -47,92 +50,67 @@ public class ClubPostController {
 	private final ClubPostService clubPostService;
 	private final ClubUserService clubUserService;
 	private final ClubRepository clubRepository;
+	private final ImageService imageService;
 
 	@GetMapping("/{clubId}/post")
 	@Operation(summary = "동아리 게시글 목록 조회 API", description = "동아리 게시글 목록을 조회하는 API 입니다.")
-	public ApiResponse<List<ClubPostListResponseDto>> findByPostType(@PathVariable Long clubId, @RequestParam(name = "category") String category) {
+	public ApiResponse<List<ClubPostListResponseDto>> findByPostType(@PathVariable Long clubId,
+		@RequestParam(name = "category") String category) {
 
 		return new ApiResponse<List<ClubPostListResponseDto>>(clubPostService.findClubPostByType(clubId, category));
 	}
-
-
-
 
 	@GetMapping("/{clubId}/post/{postId}")
 	@Operation(summary = "동아리 게시글 단건 조회 API", description = "동아리 게시글을 조회하는 API 입니다.")
 	public ApiResponse<ClubPostDto> findClubPost(@PathVariable Long postId) {
 
-
-		return new ApiResponse<ClubPostDto>(ClubPostDto.of(clubPostService.findByClubPostId(postId)));
+		return new ApiResponse<>(ClubPostDto.of(clubPostService.findByClubPostId(postId)));
 	}
 
 	@PostMapping("/{clubId}/post")
 	@Operation(summary = "동아리 게시글 생성 API", description = "동아리 게시글을 생성하는 API 입니다.")
-	public ApiResponse<ClubPostResponseDto> saveClubPost(@PathVariable Long clubId, @Valid @RequestBody ClubPostRequestDto clubPostRequestDto, @AuthenticationPrincipal CustomUserDetails userDetails){
+	public ApiResponse<ClubPostResponseDto> saveClubPost(
+		@PathVariable Long clubId,
+		@RequestPart("request") ClubPostRequestDto clubPostRequestDto,
+		@RequestPart("imageFiles") List<MultipartFile> multipartFiles,
+		@AuthenticationPrincipal CustomUserDetails userDetails
+	) {
 
 		User user = userDetails.getUser();
-		ClubPost clubPost;
+		ClubPost clubPost = clubPostService.saveClubPost(clubId, clubPostRequestDto, user);
+		imageService.saveClubPostImages(multipartFiles, clubPost.getId());
 
-		try {
-			clubPost = clubPostService.saveClubPost(clubId, clubPostRequestDto, user);
-
-		} catch (Exception exception) {
-			return new ApiResponse<>(ErrorCode.INVALID_REQUEST);
-		}
-		return new ApiResponse<ClubPostResponseDto>((ClubPostResponseDto.of(clubPost.getId(), "게시글을 성공적으로 생성하였습니다.")));
+		return new ApiResponse<>((ClubPostResponseDto.of(clubPost.getId(), "게시글을 성공적으로 생성하였습니다.")));
 	}
-
 
 	@PutMapping("/{clubId}/post/{postId}")
 	@Operation(summary = "동아리 게시글 수정 API", description = "동아리 게시글을 수정하는 API 입니다.")
-	public ApiResponse<ClubPostResponseDto> updateClubPost(@PathVariable Long clubId, @PathVariable Long postId, @Valid @RequestBody ClubPostRequestDto clubPostRequestDto, @AuthenticationPrincipal CustomUserDetails userDetails) {
+	public ApiResponse<ClubPostResponseDto> updateClubPost(
+		@PathVariable Long clubId,
+		@PathVariable Long postId,
+		@RequestPart("request") ClubPostRequestDto clubPostRequestDto,
+		@RequestPart("imageFiles") List<MultipartFile> multipartFiles,
+		@AuthenticationPrincipal CustomUserDetails userDetails) {
 
 		User user = userDetails.getUser();
-		ClubPost clubPost;
-
-		/*
-		try {
-			clubPost = clubPostService.findByClubPostId(postId);
-
-			if(clubPost==null) {
-				return new ApiResponse<>(ErrorCode.INVALID_REQUEST);
-			}
-
-			clubPostService.updateClubPost(clubPostRequestDto, user, postId);
-		} catch (Exception exception) {
-			return new ApiResponse<>(ErrorCode.INVALID_REQUEST);
-		}
-
-		 */
 
 		clubPostService.updateClubPost(clubId, clubPostRequestDto, user, postId);
+		imageService.deleteClubPostImages(postId);
+		imageService.saveClubPostImages(multipartFiles, postId);
 
-		return new ApiResponse<ClubPostResponseDto>(ClubPostResponseDto.of(postId, "게시글 상세 정보 수정이 완료되었습니다."));
+		return new ApiResponse<>(ClubPostResponseDto.of(postId, "게시글 상세 정보 수정이 완료되었습니다."));
 
 	}
 
 	@DeleteMapping("/{clubId}/post/{postId}")
 	@Operation(summary = "동아리 게시글 삭제 API", description = "동아리 게시글을 삭제하는 API 입니다.")
-	public ApiResponse deleteClubPost(@PathVariable Long clubId, @PathVariable Long postId, @AuthenticationPrincipal CustomUserDetails userDetails) {
-		try {
-			User user = userDetails.getUser();
-			ClubPost clubPost = clubPostService.findByClubPostId(postId);
+	public ApiResponse deleteClubPost(@PathVariable Long clubId, @PathVariable Long postId,
+		@AuthenticationPrincipal CustomUserDetails userDetails) {
+		User user = userDetails.getUser();
 
-			/*
-			if(clubPost==null) {
-				return new ApiResponse<>(ErrorCode.CLUB_NOT_FOUND);
-			}
+		clubPostService.deleteClubPost(clubId, postId, user);
+		imageService.deleteClubPostImages(postId);
 
-			if (!clubPost.getClubUser().getClubUserId().equals(clubUserService.clubUserFind(user.getId(), clubId).getClubUserId())) {
-				return new ApiResponse<>(ErrorCode.USER_NOT_AUTHENTICATED);
-			}
-
-			 */
-			clubPostService.deleteClubPost(clubId, postId, user);
-		} catch (Exception exception) {
-			return new ApiResponse<>(ErrorCode.INVALID_REQUEST);
-		}
 		return new ApiResponse<>(ErrorCode.REQUEST_OK);
 	}
-
 }
