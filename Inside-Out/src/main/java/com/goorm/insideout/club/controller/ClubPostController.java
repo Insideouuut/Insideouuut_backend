e4310b58894c914ac7f63333fd51332d157adbd5
jae woo Jung <awfjol2008@gmail.com>
2024-08-09 21:47:26 +0900

    feat: 동아리 게시글 수정 API METHOD 변경

--- Inside-Out/src/main/java/com/goorm/insideout/club/controller/ClubPostController.java
+++ Inside-Out/src/main/java/com/goorm/insideout/club/controller/ClubPostController.java
@@ -5,28 +5,19 @@ import java.util.List;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.goorm.insideout.auth.dto.CustomUserDetails;
-import com.goorm.insideout.chatroom.domain.ChatRoom;
-import com.goorm.insideout.chatroom.domain.ChatRoomType;
 import com.goorm.insideout.club.dto.ClubPostDto;
 import com.goorm.insideout.club.dto.requestDto.ClubPostRequestDto;
-import com.goorm.insideout.club.dto.requestDto.ClubRequestDto;
-import com.goorm.insideout.club.dto.responseDto.ClubBoardResponseDto;
-import com.goorm.insideout.club.dto.responseDto.ClubListResponseDto;
 import com.goorm.insideout.club.dto.responseDto.ClubPostListResponseDto;
 import com.goorm.insideout.club.dto.responseDto.ClubPostResponseDto;
-import com.goorm.insideout.club.dto.responseDto.ClubResponseDto;
-import com.goorm.insideout.club.entity.Club;
 import com.goorm.insideout.club.entity.ClubPost;
 import com.goorm.insideout.club.repository.ClubRepository;
 import com.goorm.insideout.club.service.ClubPostService;
@@ -38,7 +29,6 @@ import com.goorm.insideout.user.domain.User;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -82,7 +72,7 @@ public class ClubPostController {
 		return new ApiResponse<>((ClubPostResponseDto.of(clubPost.getId(), "게시글을 성공적으로 생성하였습니다.")));
 	}
 
-	@PutMapping("/{clubId}/posts/{postId}")
+	@PatchMapping("/{clubId}/posts/{postId}")
 	@Operation(summary = "동아리 게시글 수정 API", description = "동아리 게시글을 수정하는 API 입니다.")
 	public ApiResponse<ClubPostResponseDto> updateClubPost(
 		@PathVariable Long clubId,
