17fb84f914666a6d2a587f68cb2c641286dfcb32
jeongdong99 <sjd0219@gmail.com>
2024-08-11 05:54:23 +0900

    feat : 질문 답변 맵핑과 그에 따른 추가 기능

---
+++ Inside-Out/src/main/java/com/goorm/insideout/meeting/dto/AnswerDto.java
@@ -0,0 +1,11 @@
+package com.goorm.insideout.meeting.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnswerDto {
+	private String question;
+	private String answer;
+}
