a900951f327b5a1bc9d70f316fbc6d0d661ebd91
sunnybinny <yusun4756@gmail.com>
2024-08-11 22:55:26 +0900

    fix: 클럽 엔티티 수정

--- Inside-Out/src/main/java/com/goorm/insideout/club/entity/Club.java
+++ Inside-Out/src/main/java/com/goorm/insideout/club/entity/Club.java
@@ -22,6 +22,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -43,7 +44,7 @@ import lombok.Setter;
 public class Club {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "club_Id")
 	@JsonIgnore
 	private Long clubId;
--- Inside-Out/src/main/java/com/goorm/insideout/club/entity/ClubApply.java
+++ Inside-Out/src/main/java/com/goorm/insideout/club/entity/ClubApply.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -34,7 +35,7 @@ import lombok.Setter;
 public class ClubApply {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "apply_Id")
 	private Long applyId;
 
