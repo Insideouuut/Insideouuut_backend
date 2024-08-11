779862b5ab0f7a31b5b0c475eeb73142784648a3
jae woo Jung <awfjol2008@gmail.com>
2024-08-11 19:57:20 +0900

    feat: 모임 타입 관련 DTO 로직 수정

--- Inside-Out/src/main/java/com/goorm/insideout/meeting/domain/Type.java
+++ Inside-Out/src/main/java/com/goorm/insideout/meeting/domain/Type.java
@@ -1,5 +1,10 @@
 package com.goorm.insideout.meeting.domain;
 
+import java.util.Arrays;
+
+import com.goorm.insideout.global.exception.ErrorCode;
+import com.goorm.insideout.global.exception.ModongException;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,4 +15,11 @@ public enum Type {
 	CLUB_MEETING("동아리 모임");
 
 	private final String name;
+
+	public static Type findByName(String name) {
+		return Arrays.stream(Type.values())
+			.filter(type -> type.getName().equals(name))
+			.findAny()
+			.orElseThrow(() -> ModongException.from(ErrorCode.MEETING_TYPE_NOT_FOUND));
+	}
 }
