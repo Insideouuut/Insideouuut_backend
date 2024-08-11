0321642ecbe28487c0f728f0772b5971eef0e5f0
sunnybinny <yusun4756@gmail.com>
2024-08-11 21:46:09 +0900

    feat: 이미지 변수 추가

--- Inside-Out/src/main/java/com/goorm/insideout/club/dto/responseDto/ClubPostListResponseDto.java
+++ Inside-Out/src/main/java/com/goorm/insideout/club/dto/responseDto/ClubPostListResponseDto.java
@@ -1,8 +1,10 @@
 package com.goorm.insideout.club.dto.responseDto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.goorm.insideout.club.entity.ClubPost;
+import com.goorm.insideout.image.dto.response.ImageResponse;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -15,6 +17,7 @@ public class ClubPostListResponseDto {
 	private final LocalDateTime createTime;
 	private final String writer;
 	private final String content;
+	private List<ImageResponse> images;
 
 	public static ClubPostListResponseDto of(ClubPost clubPost){
 		return new ClubPostListResponseDto(clubPost);
@@ -27,5 +30,9 @@ public class ClubPostListResponseDto {
 		this.createTime = clubPost.getCreateTime();
 		this.writer = clubPost.getWriter();
 		this.content = clubPost.getPostContent();
+		this.images = clubPost.getImages()
+			.stream()
+			.map(clubPostImage -> ImageResponse.from(clubPostImage.getImage()))
+			.toList();
 	}
 }
