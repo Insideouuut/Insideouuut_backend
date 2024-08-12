d746787423734512b3663df04630b551ed12ad16
jae woo Jung <awfjol2008@gmail.com>
2024-08-13 05:18:08 +0900

    feat: 나의 동아리 모임 참여 목록 조회 메서드 구현

--- Inside-Out/src/main/java/com/goorm/insideout/meeting/repository/MeetingUserRepository.java
+++ Inside-Out/src/main/java/com/goorm/insideout/meeting/repository/MeetingUserRepository.java
@@ -11,8 +11,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.goorm.insideout.meeting.domain.MeetingUser;
 import com.goorm.insideout.meeting.domain.Progress;
 
-import io.swagger.v3.oas.annotations.Operation;
-
 public interface MeetingUserRepository extends JpaRepository<MeetingUser, Long> {
 	List<MeetingUser> findByMeetingId(Long meetingId);
 	Optional<MeetingUser> findByMeetingIdAndUserId(Long meetingId, Long userId);
@@ -22,6 +20,10 @@ public interface MeetingUserRepository extends JpaRepository<MeetingUser, Long> 
 
 	@Query("select m from MeetingUser m where m.user.id = ?1  and m.meeting.progress = ?2")
 	List<MeetingUser> findOngoingMeetingsByProgress(Long id, Progress progress);
+
+	// 나의 동아리 모임 참여 목록 찾기
+	@Query("select m from MeetingUser m where m.user.id = ?1 and m.meeting.club.clubId = ?2 and m.meeting.progress = ?3")
+	List<MeetingUser> findOngoingClubMeetingsByProgress(Long id, Long clubId, Progress progress);
 
 	// 참여중인 모임 중, 이미 종료된 모임 참여 목록 찾기 -> 이후 Meeting 객체로 변경
 	@Query("select m from MeetingUser m where m.user.id = ?1 and m.meeting.progress = ?2")
